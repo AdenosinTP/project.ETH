b412e15d352fd345595969285a9af031de6b122e
Valerio Tettamanti <Vale@Alaska.local>
2016-12-01 14:59:36 +0100

    Added files R

---
+++ analyze_freq_tau.R
@@ -0,0 +1,91 @@
+
+
+# go on session -> set working directory -> choose directory 
+# activate library for ggplot and plyr; if you don't have it -> install.packages("ggplot2")
+
+
+library(ggplot2)
+library(plyr)
+
+# directories for files
+WD = '/Users/Vale/Documents/AAA SCUOLA/ETH/First Project/data.analysis/dataspike/changetau2/3/'
+WD1 = '/Users/Vale/Documents/AAA SCUOLA/ETH/First Project/data.analysis/dataspike/changetau2/4/'
+
+# Function for reading the files; you can copy and paste for more than one 
+files = list.files(path = WD, pattern = "*.txt")
+files1 = list.files(path = WD1, pattern = "*.txt")
+n = length(files)
+# 
+
+setwd(WD)
+freq.raw = lapply(files, function(x) {
+  file = read.table(x, header = T)
+  
+  freq_all= rep(NA, 255)          # create an empty array long 255 full of zeros
+  
+  for (i in 0:255) {             # loop for the neurons (here total of 256 neurons)
+    time_n = file$time[file$core_id==0 & file$neuron_id==i]  # select the time for different neuron id
+    spikes = length(time_n)
+    min = min(time_n)
+    max = max(time_n)
+    dtime.s = (max - min)/1000000
+    freq = spikes/(dtime.s)
+    freq_all[i] = freq          # exits the  frequency of neuron_id = i for the i neuron
+  }
+  
+  
+  # in order to create the boxplot; taking quantiles and max, min
+  y0 = min(freq_all)
+  y25 = quantile(freq_all, 0.25)
+  y50 = median(freq_all)
+  y75 = quantile(freq_all, 0.75)
+  y100 = max(freq_all)
+  
+  mean_freq = mean(freq_all) # mean freq of all neurons
+  
+  return(data.frame(mean_freq, y0, y25, y50, y75, y100))  # the functions returns a list with the data
+  
+})
+
+setwd(WD1)
+freq.raw1 = lapply(files1, function(x) {
+  file = read.table(x, header = T)
+  
+  freq_all= rep(NA, 255)          # create an empty array long 255 full of zeros
+  
+  for (i in 0:255) {             # loop for the neurons (here total of 256 neurons)
+    time_n = file$time[file$core_id==0 & file$neuron_id==i]  # select the time for different neuron id
+    spikes = length(time_n)
+    min = min(time_n)
+    max = max(time_n)
+    dtime.s = (max - min)/1000000
+    freq = spikes/(dtime.s)
+    freq_all[i] = freq          # exits the  frequency of neuron_id = i for the i neuron
+  }
+  
+  
+  # in order to create the boxplot; taking quantiles and max, min
+  y0 = min(freq_all)
+  y25 = quantile(freq_all, 0.25)
+  y50 = median(freq_all)
+  y75 = quantile(freq_all, 0.75)
+  y100 = max(freq_all)
+  
+  mean_freq = mean(freq_all) # mean freq of all neurons
+  
+  return(data.frame(mean_freq, y0, y25, y50, y75, y100))  # the functions returns a list with the data
+  
+})
+
+df = ldply(freq.raw, data.frame) # the function return a list -> takes the list and makes a data frame 
+df1 = ldply(freq.raw1, data.frame)
+
+df = cbind(df, data_n=c(2,4,8,16,32,64,128,255)) # number of files you are using, in my case 8 files; 0:255 by 32
+df1 = cbind(df1, data_n=c(2,4,8,16,32,64,128,255))
+
+ggplot() + geom_line(data=df, aes(data_n, mean_freq), colour= 'red') + 
+  geom_point(data=df, aes(data_n, mean_freq)) +
+  geom_line(data=df1, aes(data_n, mean_freq), colour= 'green') + 
+  geom_point(data=df1, aes(data_n, mean_freq)) +
+  labs(title = "Mean Frequencies of the 256 Neurons for different fValues with cV of DC=0") +
+  labs(x="fValue", y = "Mean frequency [Hz]")
